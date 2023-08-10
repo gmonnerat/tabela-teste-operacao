d5cf8e9a4b2084c592345b08431c64eb28a9eaa6
Felipe Palmeira <felipedpaula97@gmail.com>
2023-08-10 11:16:26 -0300

    Cálculo da segunda tabela finalizado

--- scripts.js
+++ scripts.js
@@ -35,8 +35,92 @@ const resultLucroLiquidoOperacao = document.querySelector('#lucro-liquido-operac
 const resultLucroLiquidoAnimal = document.querySelector('#lucro-liquido-animal');
 const resultTaxaLiquidaPeriodo = document.querySelector('#taxa-liquida-periodo');
 const resultTaxaLiquidaMes = document.querySelector('#taxa-liquida-mes');
-const resultTaxaLiquidaAno = document.querySelector('#taxa-liquida-ano');;
-    
+const resultTaxaLiquidaAno = document.querySelector('#taxa-liquida-ano');
+const td2 = document.querySelector('.td-2').innerHTML;
+const baseTaxa2 = document.querySelector('.base-taxa-2');
+const td3 = document.querySelector('.td-3').innerHTML;
+const baseTaxa3 = document.querySelector('.base-taxa-3');
+const td4 = document.querySelector('.td-4').innerHTML;
+const baseTaxa4 = document.querySelector('.base-taxa-4');
+const td5 = document.querySelector('.td-5').innerHTML;
+const baseTaxa5 = document.querySelector('.base-taxa-5');
+const td6 = document.querySelector('.td-6').innerHTML;
+const baseTaxa6 = document.querySelector('.base-taxa-6');
+const td7 = document.querySelector('.td-7').innerHTML;
+const baseTaxa7 = document.querySelector('.base-taxa-7');
+const td8 = document.querySelector('.td-8').innerHTML;
+const baseTaxa8 = document.querySelector('.base-taxa-8');
+const td9 = document.querySelector('.td-9').innerHTML;
+const baseTaxa9 = document.querySelector('.base-taxa-9');
+const td10 = document.querySelector('.td-10').innerHTML;
+const baseTaxa10 = document.querySelector('.base-taxa-10');
+const td11 = document.querySelector('.td-11').innerHTML;
+const baseTaxa11 = document.querySelector('.base-taxa-11');
+const td12 = document.querySelector('.td-12').innerHTML;
+const baseTaxa12 = document.querySelector('.base-taxa-12');
+const td13 = document.querySelector('.td-13').innerHTML;
+const baseTaxa13 = document.querySelector('.base-taxa-13');
+const td14 = document.querySelector('.td-14').innerHTML;
+const baseTaxa14 = document.querySelector('.base-taxa-14');
+const td15 = document.querySelector('.td-15').innerHTML;
+const baseTaxa15 = document.querySelector('.base-taxa-15');
+const td16 = document.querySelector('.td-16').innerHTML;
+const baseTaxa16 = document.querySelector('.base-taxa-16');
+const td17 = document.querySelector('.td-17').innerHTML;
+const baseTaxa17 = document.querySelector('.base-taxa-17');
+const td18 = document.querySelector('.td-18').innerHTML;
+const baseTaxa18 = document.querySelector('.base-taxa-18');
+const td19 = document.querySelector('.td-19').innerHTML;
+const baseTaxa19 = document.querySelector('.base-taxa-19');
+const td20 = document.querySelector('.td-20').innerHTML;
+const baseTaxa20 = document.querySelector('.base-taxa-20');
+const td21 = document.querySelector('.td-21').innerHTML;
+const baseTaxa21 = document.querySelector('.base-taxa-21');
+const td22 = document.querySelector('.td-22').innerHTML;
+const baseTaxa22 = document.querySelector('.base-taxa-22');
+const lucro2 = document.querySelector('.lucro-2');
+const lucroOperacao2 = document.querySelector('.lucro-operacao-2');
+const lucro3 = document.querySelector('.lucro-3');
+const lucroOperacao3 = document.querySelector('.lucro-operacao-3');
+const lucro4 = document.querySelector('.lucro-4');
+const lucroOperacao4 = document.querySelector('.lucro-operacao-4');
+const lucro5 = document.querySelector('.lucro-5');
+const lucroOperacao5 = document.querySelector('.lucro-operacao-5');
+const lucro6 = document.querySelector('.lucro-6');
+const lucroOperacao6 = document.querySelector('.lucro-operacao-6');
+const lucro7 = document.querySelector('.lucro-7');
+const lucroOperacao7 = document.querySelector('.lucro-operacao-7');
+const lucro8 = document.querySelector('.lucro-8');
+const lucroOperacao8 = document.querySelector('.lucro-operacao-8');
+const lucro9 = document.querySelector('.lucro-9');
+const lucroOperacao9 = document.querySelector('.lucro-operacao-9');
+const lucro10 = document.querySelector('.lucro-10');
+const lucroOperacao10 = document.querySelector('.lucro-operacao-10');
+const lucro11 = document.querySelector('.lucro-11');
+const lucroOperacao11 = document.querySelector('.lucro-operacao-11');
+const lucro12 = document.querySelector('.lucro-12');
+const lucroOperacao12 = document.querySelector('.lucro-operacao-12');
+const lucro13 = document.querySelector('.lucro-13');
+const lucroOperacao13 = document.querySelector('.lucro-operacao-13');
+const lucro14 = document.querySelector('.lucro-14');
+const lucroOperacao14 = document.querySelector('.lucro-operacao-14');
+const lucro15 = document.querySelector('.lucro-15');
+const lucroOperacao15 = document.querySelector('.lucro-operacao-15');
+const lucro16 = document.querySelector('.lucro-16');
+const lucroOperacao16 = document.querySelector('.lucro-operacao-16');
+const lucro17 = document.querySelector('.lucro-17');
+const lucroOperacao17 = document.querySelector('.lucro-operacao-17');
+const lucro18 = document.querySelector('.lucro-18');
+const lucroOperacao18 = document.querySelector('.lucro-operacao-18');
+const lucro19 = document.querySelector('.lucro-19');
+const lucroOperacao19 = document.querySelector('.lucro-operacao-19');
+const lucro20 = document.querySelector('.lucro-20');
+const lucroOperacao20 = document.querySelector('.lucro-operacao-20');
+const lucro21 = document.querySelector('.lucro-21');
+const lucroOperacao21 = document.querySelector('.lucro-operacao-21');
+const lucro22 = document.querySelector('.lucro-22');
+const lucroOperacao22 = document.querySelector('.lucro-operacao-22');
+
 // -----------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------
 
@@ -163,11 +247,41 @@ function getPontoEquilibrio(investTotal, totalArroba) {
     resultPontoEquilibrio.innerHTML = (investTotal/totalArroba).toFixed(2);
 }
 
+// Taxa Base B3 - (Taxa Base B3 * (%))
+function getTaxasBaseB3() {
+    baseTaxa2.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td2/100))).toFixed(2);
+    baseTaxa3.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td3/100))).toFixed(2);
+    baseTaxa4.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td4/100))).toFixed(2);
+    baseTaxa5.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td5/100))).toFixed(2);
+    baseTaxa6.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td6/100))).toFixed(2);
+    baseTaxa7.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td7/100))).toFixed(2);
+    baseTaxa8.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td8/100))).toFixed(2);
+    baseTaxa9.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td9/100))).toFixed(2);
+    baseTaxa10.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td10/100))).toFixed(2);
+    baseTaxa11.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td11/100))).toFixed(2);
+    baseTaxa11.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td11/100))).toFixed(2);
+    baseTaxa12.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td12/100))).toFixed(2);
+    baseTaxa13.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td13/100))).toFixed(2);
+    baseTaxa14.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td14/100))).toFixed(2);
+    baseTaxa15.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td15/100))).toFixed(2);
+    baseTaxa16.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td16/100))).toFixed(2);
+    baseTaxa17.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td17/100))).toFixed(2);
+    baseTaxa18.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td18/100))).toFixed(2);
+    baseTaxa19.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td19/100))).toFixed(2);
+    baseTaxa20.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td20/100))).toFixed(2);
+    baseTaxa21.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td21/100))).toFixed(2);
+    baseTaxa22.innerHTML = (cotacaoB3.value - (cotacaoB3.value * (td22/100))).toFixed(2);
+}
+
+// function getTestesBase() {
+// }
+
 // -----------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------
 
 // ############## EXECUÇÃO DAS FUNÇÕES ##############
 function generateResults() {
+    console.log((cotacaoB3.value - (cotacaoB3.value * (td2/100))).toFixed(2));
     const inputs = document.querySelectorAll('.required');
 
     // Flag para verificar se todos os inputs estão preenchidos
@@ -205,6 +319,7 @@ function generateResults() {
         getTaxaLiquidaPeriodo(parseFloat(resultLucroLiquidoOperacao.innerHTML), parseFloat(resultInvestTotal.innerHTML))
         getTaxaLiquidaMes(parseFloat(resultTaxaLiquidaPeriodo.innerHTML), parseFloat(periodo.value))
         getTaxaLiquidaAno(parseFloat(resultTaxaLiquidaMes.innerHTML))
+        getTaxasBaseB3();
         
         // Encerra funções -> Fim dos resultados!
     } else {
